ad56ec4082270c306e128a02c7b59ee9fe207ff6
Dheeraj.kumar <dheerajkrao1993@gmail.com>
2017-12-03 12:57:41 +0530

    switch to new version. Thanks!

--- oldversion/scripts.js
+++ oldversion/scripts.js
@@ -94,7 +94,19 @@ $(document).ready(function(){
       $(".text_github").show();
   });
     
-  
+
+var pieChart = function() {
+    $('.chart').easyPieChart({
+      scaleColor: false,
+      lineWidth: 10,
+      lineCap: 'butt',
+      barColor: '#17e7a4',
+      trackColor: "#000000",
+      size: 160,
+      animate: 1000
+    });
+  };
+pieChart();
 
 
 });
